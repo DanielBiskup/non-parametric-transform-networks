a4b06b64bf43a3bb9a91563f3e64b033e78893f4
Catherine Capellen <c.capellen@gmail.com>
2018-03-08 09:35:53 +0100

    data preprocessing for MNIST rotation experiment

---
+++ mnist_rotation.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  7 16:39:39 2018
+
+@author: catherine
+"""
+
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+max_rotation = 60 # 0,30,60,90
+translation_transform = torchvision.transforms.RandomCrop((32,32), padding=2)
+
+
+def imshow(img):
+    img = img *0.1307 + 0.3081     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+use_cuda = torch.cuda.is_available()
+
+# Training dataset
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root='.', train=True, download=True,
+                   transform=transforms.Compose([
+                       translation_transform,    
+                       torchvision.transforms.RandomRotation(max_rotation),
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])), batch_size=64, shuffle=True, num_workers=4)
+# Test dataset
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root='.', train=False, transform=transforms.Compose([
+        torchvision.transforms.RandomRotation(max_rotation),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])), batch_size=64, shuffle=True, num_workers=4)
+
+    
+# show a few images, taken from tutorial
+
+# get some random training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))    
