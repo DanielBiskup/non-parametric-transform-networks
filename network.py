b7b5797314c2aa5ee99a309beef8370bbf514e45
LabComputer <c.capellen@gmail.com>
2018-03-08 16:17:27 +0100

    Using YAML files

--- network.py
+++ network.py
@@ -4,25 +4,128 @@ Cognitive Robotics Lab
 
 Non-parameteric transformation network
 """
-
 import numpy as np
-import matplotlib.pyplot as plt
-import torch
-import torchvision
-from torchvision import datasets, transforms
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+
+def make_permutation(M,N):
+    nums = [i for i in range(M*N)]
+    NUMS = np.array(nums)
+    NUMS = np.reshape(NUMS, (M,N))
+    NUMS = NUMS.T
+    return NUMS.flatten()
 
 
+""" Non-parametric transformation network layer """
+class NPTN(nn.Module):
+    def __init__(self, M, N, G, filtersize):
+        self.M=M
+        self.N=N 
+        self.G=G
+        
+        super(NPTN, self).__init__()
+        
+        self.conv1 = nn.Conv2d(self.M, self.M*self.N*self.G, filtersize, groups=self.M) # in, out, kernel size, groups as in paper
+        self.maxpool3d = nn.MaxPool3d((self.G, 1, 1)) 
+        self.meanpool3d = nn.AvgPool3d((self.M, 1, 1)) # Is that the right pooling? - AvgPool3d?
+        
+        self.permutation = make_permutation(self.M, self.N)
 
+    def forward(self, x):
+        #print('\nShape of x ', x.size())
+        x = self.conv1(x)
+        #print('Shape after convolution', x.size())
+        x = self.maxpool3d(x)
+        #print("Shape after MaxPool3d: ", x.size()) # dimension should be M*N
+        
+        #print('permutation ', permutation)
+        x = x[:, self.permutation] # reorder channels
+        #print("Shape after Channel reordering: ", x.size())
+        x = self.meanpool3d(x)
+        #print('Shape after Mean Pooling: ', x.size())
+        return x
+
+class twoLayeredNPTN(nn.Module):
+    def __init__(self, N, G, filtersize, in_channels=3):
+        super(twoLayeredNPTN, self).__init__()
+        self.final_layer_dim = (7-np.int(filtersize/1.7))**2   # works for filtersizes 3,5,7
+        # first layer 
+        self.N = N
+        self.nptn = NPTN(in_channels, N, G, filtersize)
+        self.batchnorm = nn.BatchNorm2d(N)   # is 2d the right one?
+        self.pool = nn.MaxPool2d(2)
+        self.prelu = nn.PReLU()
+        # second layer
+        self.nptn2 = NPTN(N, 16, G, filtersize)
+        self.batchnorm2 = nn.BatchNorm2d(16) 
+        self.prelu2 = nn.PReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(16 * self.final_layer_dim, 10)
+
+    def forward(self, x):
+        x = self.nptn(x)
+        #print('x after nptn ', x.size())
+        x = self.batchnorm(x)
+        #print('batchnorm ', x.size())
+        #x = F.prelu(self.nptn(x), 0.1) 
+        x = self.pool(self.prelu(x))
+        #print('shape first layer ', x.size())
+        x = self.batchnorm2(self.nptn2(x))
+        #print('after batchnorm 2 ', x.size())
+        x = self.pool2(self.prelu2(x))
+        #print('shape second layer ', x.size())
+        
+        x = x.view(-1, 16 * self.final_layer_dim)
+        #print('shape second layer ', x.size())
+        x = F.log_softmax(self.fc1(x), dim=1)
+        #print('after softmax ', x.size())
+        return x
+    
+class twoLayeredCNN(nn.Module):
+    def __init__(self, filtersize, in_channels=3, N1=48, N2=16):
+        super(twoLayeredCNN, self).__init__()
+        self.final_layer_dim = (7-np.int(filtersize/1.7))**2   # works for filtersizes 3,5,7
+        # first layer 
+        self.conv1 = nn.Conv2d(in_channels, N1, filtersize) # TODO maybe change filter size
+        self.batchnorm = nn.BatchNorm2d(N1)   # is 2d the right one?
+        self.pool = nn.MaxPool2d(2)
+        self.prelu = nn.PReLU()
+        # second layer
+        self.conv2 = nn.Conv2d(N1, N2, filtersize)
+        self.batchnorm2 = nn.BatchNorm2d(N2) 
+        self.prelu2 = nn.PReLU()
+        self.pool2 = nn.MaxPool2d(2)
+         
+        self.fc1 = nn.Linear(16 * self.final_layer_dim, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        #print('x after conv ', x.size())
+        x = self.batchnorm(x)
+        #print('batchnorm ', x.size())
+        #x = F.prelu(self.nptn(x), 0.1) 
+        x = self.pool(self.prelu(x))
+        #print('shape first layer ', x.size())
+        x = self.batchnorm2(self.conv2(x))
+        #print('after batchnorm 2 ', x.size())
+        x = self.pool2(self.prelu2(x))
+        #print('shape second layer ', x.size())
+        
+        x = x.view(-1, 16 * self.final_layer_dim)
+        #print('shape second layer ', x.size())
+        x = F.log_softmax(self.fc1(x), dim=1)
+        #print('after softmax ', x.size())
+        return x
+
+############## TEST AREA ######################################################
+''''
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+'''
 
-"""
+'''
 print(torch.cuda.is_available()) # is not and code can not use cuda (yet?)
 
 
@@ -55,79 +158,4 @@ images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images))
 # print labels
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-"""
-
-
-def make_permutation(M,N):
-    nums = [i for i in range(M*N)]
-    NUMS = np.array(nums)
-    NUMS = np.reshape(NUMS, (M,N))
-    NUMS = NUMS.T
-    return NUMS.flatten()
-
-
-""" Non-parametric transformation network layer """ # TODO make this a layer
-class NPTN(nn.Module):
-    def __init__(self, M, N, G, filtersize):
-        self.M=M
-        self.N=N 
-        self.G=G
-        
-        super(NPTN, self).__init__()
-        
-        self.conv1 = nn.Conv2d(self.M, self.M*self.N*self.G, filtersize, groups=self.M) # in, out, kernel size, groups as in paper
-        self.maxpool3d = nn.MaxPool3d((self.G, 1, 1)) 
-        self.meanpool3d = nn.AvgPool3d((self.M, 1, 1)) # Is that the right pooling? - AvgPool3d?
-        
-        self.permutation = make_permutation(self.M, self.N)
-
-    def forward(self, x):
-        #print('\nShape of x ', x.size())
-        x = self.conv1(x)
-        #print('Shape after convolution', x.size())
-        x = self.maxpool3d(x)
-        #print("Shape after MaxPool3d: ", x.size()) # dimension should be M*N
-        
-        #print('permutation ', permutation)
-        x = x[:, self.permutation] # reorder channels
-        #print("Shape after Channel reordering: ", x.size())
-        x = self.meanpool3d(x)
-        #print('Shape after Mean Pooling: ', x.size())
-        return x
-
-
-
-
-#net = NPTN(3,16,4,5)
-#outputs = net(Variable(images))
-
-#print(outputs)
-
-
-############## Chooses optimizer and loss  ##############
-#criterion = nn.CrossEntropyLoss()
-#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-'''
-
-'''
-
-
-
-
-''' Testing area 
-# test channel reordering
-permutation = [2,1,0]
-test_tensor = torch.cat([images,images,images])
-
-imshow(torchvision.utils.make_grid(test_tensor))
-#print(test_tensor)
-print(test_tensor.size())
-reordered_t = test_tensor[:,permutation] # doesn't work
-
-#print(reordered_t)
-imshow(torchvision.utils.make_grid(reordered_t))
-
-print(reordered_t.size())
 '''
