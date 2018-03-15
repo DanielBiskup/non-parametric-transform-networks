9942ee56176894cd72988a46a87f715365dad664
LabComputer <c.capellen@gmail.com>
2018-03-15 20:51:18 +0100

    WIP: Setting up a two layer network with the new achitecture

--- newNet.py
+++ newNet.py
@@ -46,9 +46,9 @@ def make_rotation_batch(rot_deg, batch_size):
 def make_rotations(rot_deg_min, rot_deg_max, num_rots):
     return np.array([make_rotation_matrix(rot_deg) for rot_deg in np.linspace(rot_deg_min, rot_deg_max, num_rots)])
 
-class NewNPTN(nn.Module):
-    def __init__(self, M, N, G, alpha, filtersize = 5, padding=0):
-        super(NewNPTN, self).__init__()
+class RTN_CORE(nn.Module):
+    def __init__(self, M, N, G, alpha, filtersize = 5, padding=0, init_kernels = None):
+        super(RTN_CORE, self).__init__()
         
         # M: number of input channeles
         # N: number of output channels
@@ -76,9 +76,11 @@ class NewNPTN(nn.Module):
         # Rondomly initialize weights: # TODO: Maybe there is a better way to initialize
         # maybe: #w = Parameter(torch.randn(M*N, k, k))  # size(w) = ( M*N x k x k )
 
-       
-        self.w = Parameter(torch.randn(M*N, 1, k, k))  # size(w) = ( M*N x 1 x k x k )
-        
+        if init_kernels is None:
+            self.w = Parameter(torch.randn(M*N, 1, k, k))  # size(w) = ( M*N x 1 x k x k )
+        else:
+            self.w = init_kernels
+            
         # From every "template" derive G "transformed templates" by applying rotation:
         # Variable(torch.randn(8,4,3,3))
         # Step 1 – Create the G transformation matrices we whish to rotate each "template" by:
@@ -100,7 +102,7 @@ class NewNPTN(nn.Module):
         self.maxpool3d = nn.MaxPool3d((self.G, 1, 1)) 
         self.meanpool3d = nn.AvgPool3d((self.M, 1, 1)) # Is that the right pooling? - AvgPool3d?
         
-        self.permutation = make_permutation(self.M, self.N)
+        self.permutation = make_permutation(self.M, self.N) # TODO: Should this also go on the GPU as a buffer?
 
     def forward(self, x):
         # Start from w ervery time and create the others as rotation of it
@@ -128,6 +130,7 @@ class NewNPTN(nn.Module):
         MNG = self.M * self.N * self.G
         
         w_unrolled = w_perm.resize(MNG, 1, self.k, self.k) # size(w_unrolled) = ( M*N*G x (M*N*G)/(M*N*G) x k x k ) #weight – filters of shape (out_channels×in_channelsgroups×kH×kW)
+        # plot_kernels(w_unrolled, num_cols=G, title='RTN')
         
         # Convolution:
         # Use the functional (torch.nn.functional.conv2d) instead of Module
@@ -145,55 +148,151 @@ class NewNPTN(nn.Module):
         x = self.meanpool3d(x)
         #print('Shape after Mean Pooling: ', x.size())
         return x
-
-
-class TestNet(nn.Module):
-    def __init__(self, M, N, G, rot_min = -90, rot_max = +90, filtersize = 3, padding=0):
-        super(TestNet, self).__init__()
+    
+# %%
+class RTN_Layer(nn.Module):
+    def __init__(self, M, N, G, alpha = 90, k = 3, padding=0):
+        super().__init__()
 
         self.M=M
         self.N=N
         self.G=G
-        self.rot_min = rot_min
-        self.rot_max = rot_max
-        self.k = filtersize
+        self.alpha = alpha
+        self.k = k
+        #padding = int(filtersize/2) # do or don't ?
 
+        self.rot_core = RTN_CORE(M, N, G, alpha = alpha, filtersize = k, padding=0)
+        self.batchnorm = nn.BatchNorm2d(N)
+        self.prelu = nn.PReLU()
+        self.pool = nn.MaxPool2d(2)
+    
+    def forward(self, x):
+        x = self.batchnorm(self.rot_core(x))
+        #print('batchnorm ', x.size())
+        x = self.pool(self.prelu(x))
+        #print('shape first layer ', x.size())
+        return x
+
+# %%
+class twoLayeredROTNET(nn.Module):
+    def __init__(self, M=3, N1=48, N2=48 , G=3, k=5, alpha=90):
+        super().__init__()
+        self.N1 = N1
+        self.N2 = N2
+        self.M=M
+        self.G=G
+        self.alpha=alpha
+        self.k = k
+        
+        #padding = int(filtersize/2) # needed if you want to use maxpooling 3 times
         if M==3: # CIFAR
             self.input_size=(3,32,32)
         else:
             self.input_size=(1,28,28)
-
-        #padding = int(filtersize/2) # do or don't ?
         
         # first layer 
-        self.newNet = NewNPTN(M, N, G, alpha = rot_max, filtersize = k, padding=0)
-        n = self.num_flat_features(self.input_size)
+        self.rtn_layer_1 = RTN_Layer(M, N1, G, alpha, k)
+        self.rtn_layer_2 = RTN_Layer(M, N2, G, alpha, k)
         
+        n = self.num_flat_features(self.input_size) 
         self.fc1 = nn.Linear(n, 10)
-        
+
     def num_flat_features(self, input_size):
         t = Variable(torch.ones(1, *input_size))
         #print('t.size() = ' + str(t.size()))
         f = self.features(t)
         #print('Shape after convolution layers = ' + str(f.size()))
         n = int(np.prod(f.size()[1:]))
-        return n  
-    
+        return n
+
     def features(self, x):
+        #print('x.size() = ' + str(x.size()))
+        
         # first layer
-        x = self.newNet(x)
+        x = self.rtn_layer_1(x)
+        
+        # second layer
+        x = self.rtn_layer_2(x)
+       
         return x
     
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = F.log_softmax(self.fc1(x), dim=1)
-        #print('shape prediction',x.shape)
         return x
 
-#####  small run to see if weights are updated  #####
+# %% Testing
+'''
+# TEST KERNELS
+k = np.concatenate((np.ones((2,5)),np.zeros((3,5))),0)
+kn = 0.5*np.ones((5,5))
+kn[2,2] = 1
+ki = np.concatenate((0.6*np.ones((5,3)),0.3*np.ones((5,2))),1)
+kl = np.concatenate((np.ones((5,1)),np.zeros((5,1)),np.ones((5,1)),np.zeros((5,1)),np.ones((5,1))),1)
+kh = np.concatenate((np.ones((5,1)),np.zeros((5,3)),np.ones((5,1))),1)
 
-use_cuda = True
+k3d = np.array([kn,ki,k])
+k6d = np.array([kl,kn,ki,kn,k,kh])
+
+k = torch.unsqueeze(torch.unsqueeze(torch.Tensor(k),0),0)
+k3d = torch.unsqueeze(torch.Tensor(k3d),0)
+k6d = torch.unsqueeze(torch.Tensor(k6d),0)
+k = Variable(k)
+k3d = Variable(k3d)
+k6d = Variable(k6d)
+
+kd3 = k3d.permute(1,0,2,3)
+kd6 = k6d.permute(1,0,2,3)
+plot_kernels(kd3.data.numpy())
+plot_kernels(kd6.data.numpy())
+
+# NET CONF
+M = 1 # M: number of input channeles
+N = 6 # N: number of output channels
+G = 3 # G: number of translations to use.
+alpha = 90 
+k = 5 # filtersize
+
+rC = RTN_CORE(M, N, G, alpha = alpha, filtersize = k, padding=0, init_kernels=kd6)
+
+# LOAD IMAGES
+max_rotation = 60 
+# Training dataset
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root='.', train=True, download=True,
+                   transform=transforms.Compose([
+                       #torchvision.transforms.RandomCrop((32,32), padding=2),    
+                       torchvision.transforms.RandomRotation(max_rotation),
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])), batch_size=64, shuffle=True, num_workers=4)
+# Test dataset
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root='.', train=False, transform=transforms.Compose([
+        torchvision.transforms.RandomRotation(max_rotation),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])), batch_size=64, shuffle=False, num_workers=4)
+
+
+
+# show a few images, taken from tutorial
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+data_iter = iter(train_loader)
+images_mnist, labels_mnist = data_iter.next()
+
+inputs = Variable(images_mnist) # size(g) = ( G x 2 x 3 ) # TODO
+outputs = rC(inputs)
+'''
+# %%
+
+#####  small run to see if weights are updated  #####
+use_cuda = False
 plot = True
 
 # load MNIST data and rotate it
@@ -226,8 +325,9 @@ k = 3 # filtersize
 # inputs = Variable(torch.randn(G,2,3)) # size(g) = ( G x 2 x 3 ) # TODO
 #__DEV_VALUES
 
-#net = NewNPTN(M, N, G, alpha = alpha, filtersize = k, padding=0)
-net = TestNet( M, N, G, alpha, filtersize = k, padding=0)
+#net = RTN_CORE(M, N, G, alpha = alpha, filtersize = k, padding=0)
+#net = TestNet( M, N, G, alpha, filtersize = k, padding=0)
+net = twoLayeredROTNET()
 
 if use_cuda:
     net.cuda()
