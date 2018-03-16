fe55b58d7783a7834ccd26fb6ce531159184ff68
Catherine Capellen <c.capellen@gmail.com>
2018-03-16 14:21:23 +0100

    new experiment

--- results.py
+++ results.py
@@ -32,6 +32,7 @@ print('Accuracy (Not reported in original paper)')
 plt.plot([1,2,3], NPTN_accs, marker='p', label='NPTN')
 plt.plot([1],[65], color='r', marker='p', label='CNN')
 plt.xlabel('(N,G)')
+plt.xticks(x, ['(48,1)','(24,2)','(16,3)'])
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
@@ -44,25 +45,24 @@ print('Rotation Experiments on MNIST')
 print('Kernelsize=5, Rotation=60?')
 print('after 300 epochs')
 print('NPTN(18,2)  Accuracy 96.32%, Loss = 0.12038491313457489')
-print('CNN(36) Accuracy 96.28 %, Loss = 0.11692798318862915' )
+print('CNN(36)     Accuracy 96.28%, Loss = 0.11692798318862915' )
 print('\nKernelsize=5, Rotation=90')
-print('CNN(36) Accuracy 94% Loss = 0.1816810576915741')
-print('NPTN(18,2), Accuracy 94, Loss = 0.2116')
+print('CNN(36)    Accuracy 94%, Loss = 0.1816810576915741')
+print('NPTN(18,2) Accuracy 94%, Loss = 0.2116')
 
 print('\n\n')
-print('NO HORIZONTAL FLIPPING Rotation Experiments on MNIST')
+print('NO HORIZONTAL FLIPPING Rotation Experiments on MNIST, BS32')
 
 print('Rotation=90')
-print('NPTN(18,2): Test NLLLoss =  0.0738690174460411, Acc 97.72 %, BS32')
-print('CNN(36): Test NLLLoss =  0.07362796711921692, 97.69 %')
+print('NPTN(18,2): Test NLLLoss =  0.0738690174460411,  Acc 97.72 %')
+print('CNN(36):    Test NLLLoss =  0.07362796711921692, Acc 97.69 %')
 print('\nRotation=60')
 print('CNN(36) Test Nllloss = 0.0559881, Acc = 98.43')
 
 print('\n\nISSUES: Higher test acc/ lower test loss \n')
 
 print('Experiment proposals:')
-print('-- rot 60 CNN')
-print('-- 12,3? NPTN rot 60')
+print('Running: -- 12,3? NPTN rot 60')
 print('less important')
 print('-- 12,3 NPTN rot 90')
 
@@ -80,13 +80,24 @@ print('Rot Net experiments')
 print('RotNet(12,3) MNIST 60 rot,alpha=60 Test NLLloss = 0.0888415, Acc=97.18 ')
 
 print('\n\nScheduled:')
-print('NO train rotation rot 60')
+print('CIFAR comparism')
+
 print('\n\nExperiment proposals')
 print('- rot 90 comparism' )
+print('')
+
+print('\n\n ############################################################# \n\n')
+
+print('Experiments where training set is not rotated')
 
 
+print('\n\nScheduled:')
+print('NO train rotation rot 60')
 
 
+print('\n\nExperiment proposals')
+print('Baseline CNN no train rotation rot 60')
+print('rotnet higher G for rot 60')
 
 
 
